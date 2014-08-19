9d6f5b77915634b537438ee0e58f8180244eaab9
ndmacdon <nicholasdmacdonald@gmail.com>
2014-08-19 11:01:46 -0700

    Initial Commit
    Added Unity Project Files and 'default' git files.

---
+++ Assets/Instantiation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instantiation : MonoBehaviour {
+	
+	// C#
+	public Transform brick;
+	
+	void Start() {
+		Debug.Log("Entered Start");
+		StartCoroutine (spawnBrick ());
+	}
+
+	IEnumerator spawnBrick() {
+		Debug.Log("Entered spawn");
+
+		for (int y = 0; y < 5; y++) {
+			for (int x = 0; x < 5; x++) {
+				Instantiate (brick, new Vector3 (x, y, 0), Quaternion.identity);
+				yield return new WaitForSeconds (2);
+			}
+		}
+	}
+}
+
---
+++ Assets/Rocket.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocket : MonoBehaviour {
+
+	// Require the rocket to be a rigidbody.
+	// This way we the user can not assign a prefab without rigidbody
+	public Rigidbody rocket;
+	public float speed = 10f;
+	
+	void FireRocket () {
+		Rigidbody rocketClone = (Rigidbody) Instantiate(rocket, transform.position, transform.rotation);
+		rocketClone.velocity = transform.forward * speed;
+		
+		// You can also acccess other components / scripts of the clone
+		//rocketClone.GetComponent<Rocket>().DoSomething();
+	}
+	
+	// Calls the fire method when holding down ctrl or mouse
+	void Update () {
+		if (Input.GetKeyDown("k")) {
+			FireRocket();
+		}
+	}
+
+}
