3e7e08aba84c9efedba92d9f296da9ac5e692af1
ndmacdon <nicholasdmacdonald@gmail.com>
2014-08-21 01:03:47 -0700

    MainMenuANDBuilderMenu
    Added skeleton for main menu.
    Added skeleton and some basic navigation controls for the
    RobotBuilderMenu.
    Set Scene order of MainMenu and RobotBuilderMenu.
    Added some fonts.

---
+++ Assets/Scripts/MainMenuGUI.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuGUI : MonoBehaviour {
+
+	public GUIStyle menuTitle;
+	public GUIStyle infoText;
+
+	void OnGUI () {
+		// Title Text
+		// TODO: Replace with texture...
+		int titleWidth = 100;
+		int titleHeight = 100;
+		GUI.Label (new Rect (Screen.width / 2 - titleWidth/2, 
+		                  Screen.height / 2 - titleHeight,
+		                  titleWidth,
+		                  titleHeight),
+		         "Crowbots",
+		           menuTitle);
+
+		// Version
+		int versionWidth = 50;
+		int versionHeight = 20;
+		GUI.Label (new Rect (Screen.width - versionWidth, 
+		                     0,
+		                     versionWidth,
+		                     versionHeight),
+		           "v 0.0.1",
+		           infoText);
+
+		// Copy Info
+		int copyWidth = 150;
+		int copyHeight = 20;
+		GUI.Label (new Rect (Screen.width - copyWidth, 
+		                     Screen.height- copyHeight,
+		                     copyWidth,
+		                     copyHeight),
+		           "SoAndSo Games",
+		           infoText);
+
+		// Menu Options
+		Vector2 menuSize = new Vector2 (100, 55);
+		Vector2 menuLoc = new Vector2 (Screen.width/2 - menuSize.x/2, Screen.height/2);
+		GUI.Box(new Rect(menuLoc.x, 
+		                 menuLoc.y,
+		                 menuSize.x,
+		                 menuSize.y),
+		        "");
+
+		int optionHeight = 20; // Height of an option-button.
+
+		// Play Button
+		if (GUI.Button (new Rect (menuLoc.x + 5,
+		                          menuLoc.y + 5,
+		                          menuSize.x - 10,
+		                          optionHeight),
+		            	"Play") ) {
+			Application.LoadLevel(1);
+		}
+
+		// Exit Button
+		if (GUI.Button (new Rect (menuLoc.x + 5,
+		                          menuLoc.y + optionHeight + 10,
+		                          menuSize.x - 10,
+		                          optionHeight),
+		            	"Exit")) {
+			Application.Quit();
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/RobotBuilderGUI.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotBuilderGUI : MonoBehaviour {
+	const int Pad = 5;
+	bool exiting = false;
+	Vector2 buttonSize = new Vector2 (70, 20);
+	Vector2 exitSize = new Vector2(155, 100);
+	Vector2 exitLoc = new Vector2(Screen.width/2 - 155/2, 
+	                              Screen.height/2 - 100/2);
+
+	void OnGUI () {
+
+		// Toolbox
+		// -------
+
+		// Selection Settings
+		// ------------------
+
+		// Camera
+		// ------
+
+		// Zoom-In Button
+		// TODO: Replace text with graphic.
+		// The first time I have ever coded a non-short-circuit condition...
+		if (Camera.main.orthographicSize > .1 & 
+		    GUI.RepeatButton (new Rect (Pad, 
+		                                Screen.height - (buttonSize.y + Pad),
+		                                buttonSize.x,
+		                                buttonSize.y),
+		                      "Zoom-In") ) {
+			Camera.main.orthographicSize -= .05f;
+		}
+
+		// Zoom-Out Button
+		// TODO: Replace text with graphic.
+		if (GUI.RepeatButton (new Rect (buttonSize.x + Pad*2, 
+		                                Screen.height - (buttonSize.y + Pad),
+		                                buttonSize.x,
+		                                buttonSize.y),
+		                      "Zoom-Out") ) {
+			Camera.main.orthographicSize += .05f;
+		}
+
+
+		// Navigation
+		// ----------
+
+		// Exit-Builder Button
+		// TODO: Replace text with graphic.
+		if (GUI.Button (new Rect (Pad,
+		                          Pad,
+		                          buttonSize.x,
+		                          buttonSize.y),
+		                "Exit") ) {
+			exiting = true;
+		}
+
+		// Confirm player wants to exit:
+		if (exiting) {
+			GUI.Box(new Rect(exitLoc.x,
+			                 exitLoc.y,
+			                 exitSize.x,
+			                 exitSize.y),
+			        "Exit?");
+			// Yes, Exit:
+			if (GUI.Button (new Rect (exitLoc.x + Pad,
+			                          exitLoc.y + 20 + Pad,
+			                          buttonSize.x,
+			                          buttonSize.x),
+			                "Confirm") ) {
+				Application.LoadLevel(0);
+			}
+
+			// No, DON'T Exit:
+			if (GUI.Button (new Rect (exitLoc.x + buttonSize.x + Pad*2,
+			                          exitLoc.y + 20 + Pad,
+			                          buttonSize.x,
+			                          buttonSize.x),
+			                "Cancel") ) {
+				exiting = false;
+			}
+
+		}
+
+
+		// Save-Robot Button
+		// TODO: Replace text with graphic.
+		if (GUI.Button (new Rect (buttonSize.x + Pad*2,
+		                          Pad,
+		                          buttonSize.x,
+		                          buttonSize.y),
+		                "Save") ) {
+			// TODO: Add save functionality...
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
